4b9c81621e86285aa3a3ef3bcde6455281004274
jonathan henshaw <jonathan.d.henshaw@gmail.com>
2019-11-21 16:30:11 +0100

    updated modules

--- visibilities_modules.py
+++ visibilities_modules.py
@@ -1,9 +1,10 @@
-from taks import concat
+execfile("/data/beegfs/astro-storage/groups/henning/henshaw/Code/distribute/tp2vis.py")
 
 def maketpvisibilities(tpimage,pointings,outputfile='tpvis.ms',lower=0,upper=-1):
     """
     Creates tp visibilities using tp2vis
     """
+    import image_analysis
     rms=image_analysis.computerms(tpimage,lower=lower,upper=upper)
     tp2vis(tpimage, outputfile, pointings, rms=rms)
 
@@ -13,5 +14,6 @@ def concatvis(vislist, outputfile='combinedvis.ms',
     """
     Preferred concat
     """
+    from tasks import concat
     concat(vis=vislist, concatvis=outputfile,
            copypointing=copypointing, timesort=timesort)
